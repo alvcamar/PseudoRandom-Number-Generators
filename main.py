f0760e9daeb93406c6bb65552ab515a65d9b0766
alvcamar <124206495+alvcamar@users.noreply.github.com>
2024-06-07 23:19:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 """
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #definimos correctamente la raiz del proyecto. Así, lo podemos ejecutar en cualquier carpeta del ordenador
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #definimos correctamente la raiz del proyecto. Así, lo podemos ejecutar en cualquier carpeta del dispositivo
 import time
 
 
